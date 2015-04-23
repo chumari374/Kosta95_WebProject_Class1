721a64c2776eb8471823c0cbdf0b48f45b9f3041
KOSTA <KOSTA@KOSTA-HP>
2015-04-23 19:35:26 +0900

    dao

--- PROJECT/src/DAO/SignDAO.java
+++ PROJECT/src/DAO/SignDAO.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import DTO.SignDTO;
 import sun.awt.SunToolkit.InfiniteLoop;
 
 public class SignDAO {
@@ -28,7 +31,7 @@ public class SignDAO {
 		}
 	}
 
-	
+	// ========== 결재문서작성에서 결재받는사람 정하기 ==============================================
 	public String getGetSign() {
 		String getGetSign_sql = "select emp.ename, grade.gradename from emp join grade on emp.grade = grade.grade "
 							  + "where grade = ?";
@@ -83,5 +86,64 @@ public class SignDAO {
 		}
 		return null;
 	}
+	
+	// ========== 결재문서작성에서 결재받는사람 정하기 끝==============================================
+	
+	
+	
+	// ========== 받은 결재 문서에서 나한테 결재 보내준 사람 보기========================
+	public List getGetSignList(int page, int limit) {
+		String getGetSignList_sql = "select * from sign where getsign = empno";
+		
+		List list = new ArrayList();
+		
+		int startrow = (page - 1) * 10 + 1;
+		int endrow = startrow + limit - 1;
+		
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(getGetSignList_sql);
+			pstmt.setInt(1, startrow);
+			pstmt.setInt(2, endrow);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				SignDTO SignBoard = new SignDTO(); // 한 건의
+				SignBoard.setSignnum(rs.getInt("SIGNNUM"));
+				SignBoard.setStarter(rs.getInt("STARTER"));
+				SignBoard.setEmpno(rs.getInt("EMPNO"));
+				SignBoard.setGetsign(rs.getInt("GETSIGN"));
+				SignBoard.setTitle(rs.getString("TITLE"));
+				SignBoard.setContent(rs.getString("CONTENT"));
+				SignBoard.setWrite_date(rs.getDate("WRITE_DATE"));
+				SignBoard.setRef(rs.getInt("REF"));
+				// SignBoard.setStep(rs.getInt("STEP"));
+				SignBoard.setStatus(rs.getString("STATUS"));
+				list.add(SignBoard); // key point (여러건의 데이터 collection사용)
+			}
+			
+			return list;
+		} catch (Exception ex) {
+			System.out.println("SignBoard 에러 : " + ex);
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+				}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+				}
+			if (conn != null)
+				try {
+					conn.close();
+				} catch (SQLException ex) {
+				}
+		}
+		return null;
+	}
+	// ========== 받은 결재 문서에서 나한테 결재 보내준 사람 보기 끝========================
 
 }
