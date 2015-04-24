31b3b34128aabca0fbd14400e7d13626e7c6c3d2
smlee <smlee@smlee.co.kr>
2015-04-24 14:25:13 +0900

    Domain_SetDTO

---
+++ PROJECT/src/DTO/Domain_SetDTO.java
@@ -0,0 +1,27 @@
+package DTO;
+
+import java.sql.Date;
+
+public class Domain_SetDTO {
+	private int empno;
+	private String emptel;
+	private Date hiredate;
+	private Date firedate;
+	private String sex;
+	private Date birth;
+	private String exist;
+	
+	public Domain_SetDTO(){}
+	
+	public Domain_SetDTO(int empno, String emptel,
+			Date hiredate, Date firedate, String sex,
+			Date birth, String exitst){
+		this.empno = empno;
+		this.emptel = emptel;
+		this.hiredate = hiredate;
+		this.firedate = firedate;
+		this.sex = sex;
+		this.birth = birth;
+		this.exist = exitst;
+	}
+}
