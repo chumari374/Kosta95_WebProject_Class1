ab80af3eb44e16361f8abf7984a18d2b3a8897dd
KOSTA <KOSTA@KOSTA-HP>
2015-04-24 16:21:10 +0900

    한느중

--- PROJECT/src/Action/GetSignListAction.java
+++ PROJECT/src/Action/GetSignListAction.java
@@ -1,5 +1,18 @@
 package Action;
 
-public class GetSignListAction {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.SignDAO;
+
+public class GetSignListAction implements Action {
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		SignDAO signdao = new SignDAO();
+		List getsignlist = new ArrayList();
+		return null;
+	}
 }
