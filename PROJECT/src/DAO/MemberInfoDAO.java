d55df209d559f189a4d60197b40115abab075a68
naturej <natu_rej@naver.com>
2015-04-27 16:24:46 +0900

    memberinfo DAO, DTO

---
+++ PROJECT/src/DAO/MemberInfoDAO.java
@@ -0,0 +1,69 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import DTO.MemberInfoDTO;
+import DTO.MessageDTO;
+
+public class MemberInfoDAO {
+	
+	DataSource ds;
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	public MemberInfoDAO() {
+		try {
+			Context context = new InitialContext();
+			ds = (DataSource) context.lookup("java:comp/env/jdbc/oracle");
+		} catch (Exception e) {
+			System.out.println("DB연결 실패:" + e);
+			return;
+		}
+	}
+	
+	// 맴버정보출력
+	public MemberInfoDTO getMemberInfo(int num){
+		
+		MemberInfoDTO memberinfo = null;
+		
+		try{
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement("select empno, ename, celphone, email, deptname, "
+					+ "teamname, gradename, emptel, p_picture, p_content"
+					+ "from memberinfo where e.empno = ?");
+			pstmt.setInt(1, num);
+			
+			rs= pstmt.executeQuery();
+			
+			if(rs.next()){
+				memberinfo = new MemberInfoDTO();
+				memberinfo.setP_picture(rs.getString("P_PICTURE"));
+				memberinfo.setP_content(rs.getString("P_CONTENT"));
+				memberinfo.setEmpno(rs.getInt("EMPNO"));
+				memberinfo.setEname(rs.getString("ENAME"));
+				memberinfo.setCelphone(rs.getString("CELPHONE"));
+				memberinfo.setEmail(rs.getString("EMAIL"));
+				memberinfo.setDeptname(rs.getString("DEPTNAME"));
+				memberinfo.setTeamname(rs.getString("TEAMNAME"));
+				memberinfo.setGradename(rs.getString("GRADENAME"));
+				memberinfo.setEmptel(rs.getString("EMPTEL"));
+			}
+			return memberinfo;
+		}catch(Exception ex){
+			System.out.println("getMemberInfo 에러 : " + ex);
+		}finally{
+			if(rs!=null)try{rs.close();}catch(SQLException ex){}
+			if(pstmt !=null)try{pstmt.close();}catch(SQLException ex){}
+			if(conn !=null)try{conn.close();}catch(SQLException ex){}
+		}
+		return null;
+	}
+}
