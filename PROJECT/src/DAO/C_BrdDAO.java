8e4d4d7b1393273aad61a9b3d542b7d83811ef7a
HongSoonho <0113784358@naver.com>
2015-04-27 19:36:43 +0900

    ASDDZXC

--- PROJECT/src/DAO/C_BrdDAO.java
+++ PROJECT/src/DAO/C_BrdDAO.java
@@ -29,8 +29,27 @@ public class C_BrdDAO {
 		}
 	}
 	
-	// 게시판 리스트 출력
-	public List getList(int page, int limit){
+	//조회수 업데이트.
+	public void setReadCountUpdate(int num) throws Exception{
+		String sql="update C_BRD set COUNT = COUNT+1 where NUM = "+num;
+		
+		try{
+			conn=ds.getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+		}catch(SQLException ex){
+			System.out.println("setReadCountUpdate 에러 : "+ex);
+		}
+		finally{
+			try{
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {}
+		}
+	}
+	
+//	 게시판 리스트 출력
+	public List getList(int page, int limit){//limit 페이지사이즈
 		// 글 목록 보기
 				/*
 				 * 18건 
@@ -42,11 +61,8 @@ public class C_BrdDAO {
 				 */
 				// List.jsp
 				// page =2 , limit =10(pagesize)
-		String getC_BrdList_sql = "select * from "
-				+ "(select rownum rnum, NUM,EMPNO,SUBJECT,"
-				+ "CONTENT,REF,DPTH,STEP,READNUM,TO_CHAR(WRITE_DATE, 'YYYY/MM/DD') AS WRITE_DATE from "
-				+ "(select * from C_BRD order by REF desc,STEP asc)) "
-				+ "where rnum>=? and rnum<=?";
+		//글번호	제목	작성자	작성일	조회수
+		String getC_BrdList_sql = "select * from (select rownum rnum, NUM,EMPNO,TITLE,CONTENT,REF,DPTH,STEP,COUNT,WRITE_DATE from (select * from C_BRD order by REF desc,STEP asc)) where rnum>=? and rnum<=?";
 		
 		List<C_BrdDTO> list = new ArrayList<C_BrdDTO>();
 		
@@ -77,7 +93,8 @@ public class C_BrdDAO {
 						c_brd.setRef(rs.getInt("REF"));
 						c_brd.setDpth(rs.getInt("DPTH"));
 						c_brd.setStep(rs.getInt("STEP"));
-						
+						c_brd.setCount(rs.getInt("COUNT"));
+						c_brd.setEname(rs.getString("ENAME"));
 						list.add(c_brd);
 					}
 
@@ -100,10 +117,8 @@ public class C_BrdDAO {
 		
 		try {
 			conn = ds.getConnection();
-			pstmt = conn
-					.prepareStatement("select BOARD_NUM,BOARD_NAME,BOARD_SUBJECT,"
-				+ "BOARD_CONTENT,BOARD_FILE,BOARD_RE_REF,BOARD_RE_LEV,"
-				+ "BOARD_RE_SEQ,BOARD_READCOUNT,TO_CHAR(BOARD_DATE, 'YYYY/MM/DD') AS BOARD_DATE from board where BOARD_NUM = ?");
+			String sql = "select * from c_brd where num = ?";
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, num);
 
 			rs = pstmt.executeQuery();
@@ -119,6 +134,8 @@ public class C_BrdDAO {
 				c_brd.setRef(rs.getInt("REF"));
 				c_brd.setDpth(rs.getInt("DPTH"));
 				c_brd.setStep(rs.getInt("STEP"));
+				c_brd.setEname(rs.getString("ENAME"));
+				c_brd.setCount(rs.getInt("COUNT"));
 			}
 			return c_brd;
 		} catch (Exception ex) {
@@ -171,170 +188,170 @@ public class C_BrdDAO {
 	
 	// 글 답변 (key point)
 	// 글을 읽고 그 글에 대한 답변(게시글)
-	public int Reply(C_BrdDTO c_brd){
-		// 답변 2개의 쿼리
-		// 1, update ... 내가 자리잡을 위치 board_re_seq
-		// 2, insert ... 실 데이터 처리
-		// 코드 구현
-		String c_brd_max_sql = "select max(num) from c_brd";
-		String sql = "";
-		int num = 0;
-		int result = 0;
-		
-		// 현재 내가 읽고 답변을 하려는 원본글의 정보
-		int ref = c_brd.getRef();
-		int dpth = c_brd.getDpth();
-		int step = c_brd.getStep();
-		
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(c_brd_max_sql);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				num = rs.getInt(1) + 1;
-			} else {
-				num = 1;
-			}
-			// update 문 (위치 확보)
-			sql = " UPDATE c_brd SET STEP = STEP +1 "
-					+ " WHERE REF = ? AND STEP > ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, ref);
-			pstmt.setInt(2, dpth);
-
-			result = pstmt.executeUpdate();
-
-			// insert board :(key point)
-			step = step + 1; // 현재 읽은 글 + 1
-			dpth = dpth + 1; // 현재 읽은 글 + 1
-
-			sql = "insert into c_brd (NUM, EMPNO, TITLE,";
-			sql += "CONTENT, NOTICE, WRITE_DATE, REF, DPTH, STEP)"
-					+ "values(?,?,?,?,?,?,?,?,?,?,sysdate)";
-			
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			pstmt.setInt(2, c_brd.getEmpno());
-			pstmt.setString(3, c_brd.getTitle());
-			pstmt.setString(4, c_brd.getContent());
-			pstmt.setString(5, c_brd.getNotice());
-			pstmt.setDate(6, c_brd.getWrite_date());
-			pstmt.setInt(7, ref);
-			pstmt.setInt(8, dpth);
-			pstmt.setInt(9, step);
-			pstmt.executeUpdate();
-			
-			return num;
-		} catch (SQLException e) {
-			System.out.println("Reply Error : " + e.getMessage());
-		} finally {
-			if (rs != null) try { rs.close(); } catch (SQLException ex) {}
-			if (pstmt != null) try { pstmt.close(); } catch (SQLException ex) {}
-			if (conn != null) try { conn.close(); } catch (SQLException ex) {}
-		}
-		return 0;
-	}
-	
-	// 글 수정
-	public boolean Modify(C_BrdDTO modifyc_brd){
-		
-		String sql = "update c_brd set TITLE=?, CONTENT=? where NUM=?";
-		
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, modifyc_brd.getTitle());
-			pstmt.setString(2, modifyc_brd.getContent());
-			pstmt.setInt(3, modifyc_brd.getNum());
-			pstmt.executeUpdate();
-			return true;
-		} catch (Exception ex) {
-			System.out.println("boardModify 에러 : " + ex);
-		} finally {
-			if (rs != null) try { rs.close(); } catch (SQLException ex) {}
-			if (pstmt != null) try { pstmt.close(); } catch (SQLException ex) {}
-			if (conn != null) try { conn.close(); } catch (SQLException ex) {}
-		}
-		return false;
-	}
-	
-	// 글 삭제
-		// delete.jsp
-		public boolean Delete(int num) {
-			// 주의점
-			// 규칙 : 원본글이 삭제되면 다 지워 (ref = 1 ) .....
-
-			// 규칙 : state 컬럼 : T , F 글이 삭제하는 것이 아니라 상태
-			// : update set state = F where num=1
-			// : F (삭제된 글) 문자만
-
-			// 규칙 : 원본글 답변 지워지지 않고 답변 이 있으면 삭제 금지
-			// : 답변 1개라도 있으면 원본글(삭제)
-			// : 답변에 답변이 1개라도 있다면 삭제금지
-
-			// Table column : state : t , f
-			// 원본글
-			// ->답변 삭제 (x) 답변
-			// ->답변_1
-			String c_brd_delete_sql = "delete from c_brd where num=?";
-
-			int result = 0;
-
-			try {
-				conn = ds.getConnection();
-				pstmt = conn.prepareStatement(c_brd_delete_sql);
-				pstmt.setInt(1, num);
-				result = pstmt.executeUpdate();
-				if (result == 0)
-					return false;
-
-				return true;
-			} catch (Exception ex) {
-				System.out.println("boardDelete 에러 : " + ex);
-			} finally {
-				try {
-					if (pstmt != null) pstmt.close();
-					if (conn != null) conn.close();
-				} catch (Exception ex) {
-					
-				}
-			}
-
-			return false;
-		}
-		
-		// 조회수 업데이트
-		// 테이블에 조회수가 없다..!!
-		
-		
-		
-		// 글쓴이인지 확인
-		// 맞는지 확실하지 않음...
-		public boolean isboardWriter(int num, String empno){
-			
-			String board_sql = "select * from c_brd where NUM=?";
-			
-			try{
-				conn = ds.getConnection();
-				pstmt = conn.prepareStatement(board_sql);
-				pstmt.setInt(1, num);
-				rs=pstmt.executeQuery();
-				rs.next();
-				
-				if(empno.equals(rs.getString("EMPNO"))){
-					return true;
-				}
-			}catch(SQLException ex){
-				System.out.println("isBoardWriter 에러 : "+ex);
-			}
-			finally{
-				try{
-					if(pstmt!=null)pstmt.close();
-					if(conn!=null)conn.close();
-				}
-				catch(Exception ex) {}
-			}
-			return false;
-		}
+//	public int Reply(C_BrdDTO c_brd){
+//		// 답변 2개의 쿼리
+//		// 1, update ... 내가 자리잡을 위치 board_re_seq
+//		// 2, insert ... 실 데이터 처리
+//		// 코드 구현
+//		String c_brd_max_sql = "select max(num) from c_brd";
+//		String sql = "";
+//		int num = 0;
+//		int result = 0;
+//		
+//		// 현재 내가 읽고 답변을 하려는 원본글의 정보
+//		int ref = c_brd.getRef();
+//		int dpth = c_brd.getDpth();
+//		int step = c_brd.getStep();
+//		
+//		try {
+//			conn = ds.getConnection();
+//			pstmt = conn.prepareStatement(c_brd_max_sql);
+//			rs = pstmt.executeQuery();
+//			if (rs.next()) {
+//				num = rs.getInt(1) + 1;
+//			} else {
+//				num = 1;
+//			}
+//			// update 문 (위치 확보)
+//			sql = " UPDATE c_brd SET STEP = STEP +1 "
+//					+ " WHERE REF = ? AND STEP > ?";
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setInt(1, ref);
+//			pstmt.setInt(2, dpth);
+//
+//			result = pstmt.executeUpdate();
+//
+//			// insert board :(key point)
+//			step = step + 1; // 현재 읽은 글 + 1
+//			dpth = dpth + 1; // 현재 읽은 글 + 1
+//
+//			sql = "insert into c_brd (NUM, EMPNO, TITLE,";
+//			sql += "CONTENT, NOTICE, WRITE_DATE, REF, DPTH, STEP)"
+//					+ "values(?,?,?,?,?,?,?,?,?,?,sysdate)";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setInt(1, num);
+//			pstmt.setInt(2, c_brd.getEmpno());
+//			pstmt.setString(3, c_brd.getTitle());
+//			pstmt.setString(4, c_brd.getContent());
+//			pstmt.setString(5, c_brd.getNotice());
+//			pstmt.setDate(6, c_brd.getWrite_date());
+//			pstmt.setInt(7, ref);
+//			pstmt.setInt(8, dpth);
+//			pstmt.setInt(9, step);
+//			pstmt.executeUpdate();
+//			
+//			return num;
+//		} catch (SQLException e) {
+//			System.out.println("Reply Error : " + e.getMessage());
+//		} finally {
+//			if (rs != null) try { rs.close(); } catch (SQLException ex) {}
+//			if (pstmt != null) try { pstmt.close(); } catch (SQLException ex) {}
+//			if (conn != null) try { conn.close(); } catch (SQLException ex) {}
+//		}
+//		return 0;
+//	}
+//	
+//	// 글 수정
+//	public boolean Modify(C_BrdDTO modifyc_brd){
+//		
+//		String sql = "update c_brd set TITLE=?, CONTENT=? where NUM=?";
+//		
+//		try {
+//			conn = ds.getConnection();
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, modifyc_brd.getTitle());
+//			pstmt.setString(2, modifyc_brd.getContent());
+//			pstmt.setInt(3, modifyc_brd.getNum());
+//			pstmt.executeUpdate();
+//			return true;
+//		} catch (Exception ex) {
+//			System.out.println("boardModify 에러 : " + ex);
+//		} finally {
+//			if (rs != null) try { rs.close(); } catch (SQLException ex) {}
+//			if (pstmt != null) try { pstmt.close(); } catch (SQLException ex) {}
+//			if (conn != null) try { conn.close(); } catch (SQLException ex) {}
+//		}
+//		return false;
+//	}
+//	
+//	// 글 삭제
+//		// delete.jsp
+//		public boolean Delete(int num) {
+//			// 주의점
+//			// 규칙 : 원본글이 삭제되면 다 지워 (ref = 1 ) .....
+//
+//			// 규칙 : state 컬럼 : T , F 글이 삭제하는 것이 아니라 상태
+//			// : update set state = F where num=1
+//			// : F (삭제된 글) 문자만
+//
+//			// 규칙 : 원본글 답변 지워지지 않고 답변 이 있으면 삭제 금지
+//			// : 답변 1개라도 있으면 원본글(삭제)
+//			// : 답변에 답변이 1개라도 있다면 삭제금지
+//
+//			// Table column : state : t , f
+//			// 원본글
+//			// ->답변 삭제 (x) 답변
+//			// ->답변_1
+//			String c_brd_delete_sql = "delete from c_brd where num=?";
+//
+//			int result = 0;
+//
+//			try {
+//				conn = ds.getConnection();
+//				pstmt = conn.prepareStatement(c_brd_delete_sql);
+//				pstmt.setInt(1, num);
+//				result = pstmt.executeUpdate();
+//				if (result == 0)
+//					return false;
+//
+//				return true;
+//			} catch (Exception ex) {
+//				System.out.println("boardDelete 에러 : " + ex);
+//			} finally {
+//				try {
+//					if (pstmt != null) pstmt.close();
+//					if (conn != null) conn.close();
+//				} catch (Exception ex) {
+//					
+//				}
+//			}
+//
+//			return false;
+//		}
+//		
+//		// 조회수 업데이트
+//		// 테이블에 조회수가 없다..!!
+//		
+//		
+//		
+//		// 글쓴이인지 확인
+//		// 맞는지 확실하지 않음...
+//		public boolean isboardWriter(int num, String empno){
+//			
+//			String board_sql = "select * from c_brd where NUM=?";
+//			
+//			try{
+//				conn = ds.getConnection();
+//				pstmt = conn.prepareStatement(board_sql);
+//				pstmt.setInt(1, num);
+//				rs=pstmt.executeQuery();
+//				rs.next();
+//				
+//				if(empno.equals(rs.getString("EMPNO"))){
+//					return true;
+//				}
+//			}catch(SQLException ex){
+//				System.out.println("isBoardWriter 에러 : "+ex);
+//			}
+//			finally{
+//				try{
+//					if(pstmt!=null)pstmt.close();
+//					if(conn!=null)conn.close();
+//				}
+//				catch(Exception ex) {}
+//			}
+//			return false;
+//		}
 		
 }
