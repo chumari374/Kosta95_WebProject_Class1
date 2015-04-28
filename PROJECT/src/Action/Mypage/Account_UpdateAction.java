615b807a477d78e0eb17f32d4961facc3804a60b
smlee <smlee@smlee.co.kr>
2015-04-28 18:06:03 +0900

    vd

--- PROJECT/src/Action/Mypage/Account_UpdateAction.java
+++ PROJECT/src/Action/Mypage/Account_UpdateAction.java
@@ -22,21 +22,23 @@ public class Account_UpdateAction implements Action{
 		MultipartRequest multi =  new MultipartRequest(
 	      	      request, //jsp가지는 request(요청객체) 얻어오기 
 	      	      "Upload" , // 저장경로 (물리적 경로)
-	      	    10*1024*1024 , //파일크기(한번에 업로드 할수 있는 파일의 크기)10M 
+	      	      10*1024*1024 , //파일크기(한번에 업로드 할수 있는 파일의 크기)10M 
 	      	      "utf-8", //인코딩 처리 (한글)
 	      	      new DefaultFileRenamePolicy() //파일명 중복처리 객체 사용
 	      	      );
+		String file = (String)multi.getFileNames().nextElement();
+		String p_picture = multi.getFilesystemName(file);
 		
 		ActionForward forward = new ActionForward();
 		AccountsDAO dao = new AccountsDAO();
 		HttpSession session = request.getSession();
 		AccountsDTO dto = new AccountsDTO();
-		dto.setPwd(request.getParameter("pwd"));
-		dto.setEmail(request.getParameter("email"));
-		dto.setAddr(request.getParameter("addr"));
-		dto.setCelphone(request.getParameter("celphone"));
-		dto.setP_picture(request.getParameter("p_picture"));
-		dto.setP_content(request.getParameter("p_content"));
+		dto.setPwd(multi.getParameter("pwd"));
+		dto.setEmail(multi.getParameter("email"));
+		dto.setAddr(multi.getParameter("addr"));
+		dto.setCelphone(multi.getParameter("celphone"));
+		dto.setP_picture(p_picture);
+		dto.setP_content(multi.getParameter("p_content"));
 		int result = dao.AccountUpdate((int)session.getAttribute("empno"),dto);
 		System.out.println(result);
 		
