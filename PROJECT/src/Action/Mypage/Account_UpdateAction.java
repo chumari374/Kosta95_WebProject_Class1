748914e1b7a223a36598b0ccce719f19e00c379d
smlee <smlee@smlee.co.kr>
2015-04-28 18:03:04 +0900

    fw

--- PROJECT/src/Action/Mypage/Account_UpdateAction.java
+++ PROJECT/src/Action/Mypage/Account_UpdateAction.java
@@ -11,11 +11,22 @@ import Action.ActionForward;
 import DAO.AccountsDAO;
 import DTO.AccountsDTO;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 public class Account_UpdateAction implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		MultipartRequest multi =  new MultipartRequest(
+	      	      request, //jsp가지는 request(요청객체) 얻어오기 
+	      	      "Upload" , // 저장경로 (물리적 경로)
+	      	    10*1024*1024 , //파일크기(한번에 업로드 할수 있는 파일의 크기)10M 
+	      	      "utf-8", //인코딩 처리 (한글)
+	      	      new DefaultFileRenamePolicy() //파일명 중복처리 객체 사용
+	      	      );
+		
 		ActionForward forward = new ActionForward();
 		AccountsDAO dao = new AccountsDAO();
 		HttpSession session = request.getSession();
