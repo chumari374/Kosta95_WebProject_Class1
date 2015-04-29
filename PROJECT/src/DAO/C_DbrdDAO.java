9ab8d7d51ad82c0198f502297d6ddc4d78c7e71c
smlee <smlee@smlee.co.kr>
2015-04-29 09:07:09 +0900

    ㅊㅊ

--- PROJECT/src/DAO/C_DbrdDAO.java
+++ PROJECT/src/DAO/C_DbrdDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -25,6 +27,84 @@ public class C_DbrdDAO {
 			System.out.println("DB연결 실패:" + e);
 			return;
 		}
+	}
+	
+	public int getListCount() {
+		// select count(*) from board
+		int rowcount = 0;
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement("select count(*) from C_DBRD");
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				rowcount = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {rs.close();} catch (SQLException s) {}
+			try {pstmt.close();} catch (SQLException s) {}
+			try {conn.close();} catch (SQLException s) {}
+		}
+		return rowcount;
+	}
+	
+	public List getDataBoardList(int page, int limit){//limit 페이지사이즈
+		// 글 목록 보기
+		/*
+		 * 18건 
+		 * pagesize = 5 
+		 * pagelist = 4개        1~5 , 5~10 , 11~15 , 16~18 
+		 * pageindex = 3        11~15개의 데이터
+		 * 
+		 * BoardBean page = new BoardBean(); =>Table Row 한건 여러건의 데이터 배열
+		 */
+		// List.jsp
+		// page =2 , limit =10(pagesize)
+		//글번호	제목	작성자	작성일	조회수
+		//int limit = 10;
+		String getC_DbrdList_sql = "select * from (select rownum rnum, NUM,EMPNO,TITLE,CONTENT,WRITE_DATE,DATA from C_DBRD) where rnum>=? and rnum<=?";
+		
+		List<C_DbrdDTO> list = new ArrayList<C_DbrdDTO>();
+		
+		// List<Article> articleList = new ArrayList<Article>();
+		// 권장방식 generic 사용
+		int startrow = (page - 1) * 10 + 1;
+		// Code //읽기 시작할 row 번호.
+		// (2-1)*10+1 (1)*10+1 10 +1 11
+		// page 없어 : 1
+		int endrow = startrow + limit - 1; // 읽을 마지막 row 번호.
+		// 11 +10 => 21-1 => 20
+		// page 없어 : 10
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(getC_DbrdList_sql);
+			pstmt.setInt(1, startrow); // 11 21 code
+			pstmt.setInt(2, endrow); // 20 30 code
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				C_DbrdDTO c_dbrd = new C_DbrdDTO();
+				c_dbrd.setNum(rs.getInt("NUM"));
+				c_dbrd.setEmpno(rs.getInt("EMPNO"));
+				c_dbrd.setTitle(rs.getString("TITLE"));
+				c_dbrd.setContent(rs.getString("CONTENT"));
+				c_dbrd.setWrite_date(rs.getDate("WRITE_DATE"));
+				c_dbrd.setData(rs.getString("DATA"));
+				list.add(c_dbrd);
+			}
+
+		} catch (Exception ex) {
+			System.out.println("getBoardList 에러 : " + ex);
+		} finally {
+			if (rs != null)
+				try {rs.close();} catch (SQLException ex) {}
+			if (pstmt != null)
+				try {pstmt.close();} catch (SQLException ex) {}
+			if (conn != null)
+				try {conn.close();} catch (SQLException ex) {}
+		}
+		return list;
 	}
 	
 	public int C_dbrdInsert(C_DbrdDTO C_Dbrd){
