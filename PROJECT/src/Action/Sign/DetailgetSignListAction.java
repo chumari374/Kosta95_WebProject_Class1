1ba6616b07a017a456fd40d6e946576cb1d41bb6
KOSTA <KOSTA@KOSTA-HP>
2015-04-29 15:12:07 +0900

    dvss

---
+++ PROJECT/src/Action/Sign/DetailgetSignListAction.java
@@ -0,0 +1,37 @@
+package Action.Sign;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Action.Action;
+import Action.ActionForward;
+import DAO.SignDAO;
+import DTO.SignDTO;
+
+public class DetailgetSignListAction implements Action {
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		int num = Integer.parseInt(request.getParameter("num"));
+		SignDAO signdao = new SignDAO();
+		SignDTO signdata = new SignDTO();
+		
+		System.out.println("액션도달");
+		
+		signdata = signdao.DetailgetSign(num);
+		
+		if(signdata == null) {
+			System.out.println("Null Data 처리");
+			return null;
+		}
+		System.out.println("SignDTO Data 완료");
+		
+		request.setAttribute("signdata", signdata);
+		
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("/Sign/Sign_(5)Receive_View.jsp");
+		return forward;
+	}
+
+}
