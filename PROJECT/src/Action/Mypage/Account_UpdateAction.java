e0482bdd5fb12abb8295925114a50fae4862e1d3
smlee <smlee@smlee.co.kr>
2015-04-29 17:05:23 +0900

    gki

--- PROJECT/src/Action/Mypage/Account_UpdateAction.java
+++ PROJECT/src/Action/Mypage/Account_UpdateAction.java
@@ -19,16 +19,12 @@ public class Account_UpdateAction implements Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		String RequestURI = request.getRequestURI();
-		String ContextPath = request.getContextPath();
-		String path = "Upload";
-		System.out.println("RequestURI : " + RequestURI);
-		System.out.println("ContextPath : " + ContextPath);
-		
+		String path = request.getRealPath("upload");
+		System.out.println("--:"+path);
 		
 		MultipartRequest multi =  new MultipartRequest(
 	      	      request, //jsp가지는 request(요청객체) 얻어오기 
-	      	      path , // 저장경로 (물리적 경로)
+	      	      "upload" , // 저장경로 (물리적 경로)
 	      	      10*1024*1024 , //파일크기(한번에 업로드 할수 있는 파일의 크기)10M 
 	      	      "utf-8", //인코딩 처리 (한글)
 	      	      new DefaultFileRenamePolicy() //파일명 중복처리 객체 사용
