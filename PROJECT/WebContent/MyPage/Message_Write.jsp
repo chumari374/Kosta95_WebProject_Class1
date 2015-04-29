1eebc52e2502a2069c62deea9f40daa54bec68ef
Choo <Kayung>
2015-04-29 15:34:37 +0900

    message

--- PROJECT/WebContent/MyPage/Message_Write.jsp
+++ PROJECT/WebContent/MyPage/Message_Write.jsp
@@ -1,3 +1,12 @@
+<%
+    String msgnum = (String.valueOf(session.getAttribute("msgnum"))); 
+	String empno = (String.valueOf(session.getAttribute("empno"))); 
+	String title = (String)session.getAttribute("title");
+	String content = (String)session.getAttribute("content");
+	String filepath = (String)session.getAttribute("filepath");
+	String sendempno = (String.valueOf(session.getAttribute("sendempno")));
+	
+%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -9,6 +18,13 @@
 	#message_write_table {border:1px solid black}
 	#message_write_table th {text-align: right}
 </style>
+
+	<script language="javascript">
+	function addboard(){
+		boardform.submit();
+	}
+	
+</script>
 </head>
 <body>
 	<div id="message_write_div">
@@ -45,6 +61,13 @@
 						<input type="file" name="filepath">
 					</td>
 				</tr>
+					<tr align="center" valign="middle">
+		<td colspan="2">
+		
+				<a href="javascript:addboard()">
+				<input type="submit" value="등록" class="btn btn-primary btn-inline-block btn-lg">
+				</a>
+				
 			</table>
 		</form>
 	</div>
--- PROJECT/src/DAO/MessageDAO.java
+++ PROJECT/src/DAO/MessageDAO.java
@@ -11,7 +11,6 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-import DTO.C_BrdDTO;
 import DTO.MessageDTO;
 
 public class MessageDAO {
@@ -64,105 +63,6 @@ public class MessageDAO {
 		}
 		return null;
 	}
-	
-	
-	
-	public int getMessageListCount() {
-		// select count(*) from board
-		int rowcount = 0;
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement("select count(*) from Message");
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				rowcount = rs.getInt(1);
-			}
-		} catch (Exception e) {
-
-		} finally {
-			try {
-				rs.close();
-			} catch (SQLException s) {
-				s.printStackTrace();
-			}
-			try {
-				pstmt.close();
-			} catch (SQLException s) {
-				s.printStackTrace();
-			}
-			try {
-				conn.close();
-			} catch (SQLException s) {
-				s.printStackTrace();
-			}
-		}
-		return rowcount;
-	}
-	
-/*//	 게시판 리스트 출력
-//	public List getList(int page, int limit){//limit 페이지사이즈
-	public List getBoardList(int page, int limit){//limit 페이지사이즈
-		// 글 목록 보기
-				
-				 * 18건 
-				 * pagesize = 5 
-				 * pagelist = 4개        1~5 , 5~10 , 11~15 , 16~18 
-				 * pageindex = 3        11~15개의 데이터
-				 * 
-				 * BoardBean page = new BoardBean(); =>Table Row 한건 여러건의 데이터 배열
-				 
-				// List.jsp
-				// page =2 , limit =10(pagesize)
-		//글번호	제목	작성자	작성일	조회수
-	//	int limit = 10;
-		String getC_BrdList_sql = "select * from (select rownum rnum, NUM,EMPNO,TITLE,CONTENT,REF,DPTH,STEP,COUNT,WRITE_DATE,ENAME,NOTICE from (select * from C_BRD order by REF desc,STEP asc)) where rnum>=? and rnum<=?";
-		
-		List<C_BrdDTO> list = new ArrayList<C_BrdDTO>();
-		
-		// List<Article> articleList = new ArrayList<Article>();
-				// 권장방식 generic 사용
-				int startrow = (page - 1) * 10 + 1;
-				// Code //읽기 시작할 row 번호.
-				// (2-1)*10+1 (1)*10+1 10 +1 11
-				// page 없어 : 1
-				int endrow = startrow + limit - 1; // 읽을 마지막 row 번호.
-				// 11 +10 => 21-1 => 20
-				// page 없어 : 10
-				try {
-					conn = ds.getConnection();
-					pstmt = conn.prepareStatement(getC_BrdList_sql);
-					pstmt.setInt(1, startrow); // 11 21 code
-					pstmt.setInt(2, endrow); // 20 30 code
-					rs = pstmt.executeQuery();
-
-					while (rs.next()) {
-						C_BrdDTO c_brd = new C_BrdDTO();
-						c_brd.setNum(rs.getInt("NUM"));
-						c_brd.setEmpno(rs.getInt("EMPNO"));
-						c_brd.setTitle(rs.getString("TITLE"));
-						c_brd.setContent(rs.getString("CONTENT"));
-						c_brd.setNotice(rs.getString("NOTICE"));
-						c_brd.setWrite_date(rs.getDate("WRITE_DATE"));
-						c_brd.setRef(rs.getInt("REF"));
-						c_brd.setDpth(rs.getInt("DPTH"));
-						c_brd.setStep(rs.getInt("STEP"));
-						c_brd.setCount(rs.getInt("COUNT"));
-						c_brd.setEname(rs.getString("ENAME"));
-						list.add(c_brd);
-					}
-
-					return list;
-				} catch (Exception ex) {
-					System.out.println("getBoardList 에러 : " + ex);
-				} finally {
-					if (rs != null) try { rs.close(); } catch (SQLException ex) {}
-					if (pstmt != null) try { pstmt.close(); } catch (SQLException ex) {}
-					if (conn != null) try { conn.close(); } catch (SQLException ex) {}
-				}
-				return null;
-		}
-	
-	*/
 	
 	// 메세지 내용보기
 	public MessageDTO getMessageDetail(int num){
@@ -267,5 +167,4 @@ public class MessageDAO {
 		
 		return false;
 	}
-
 }
