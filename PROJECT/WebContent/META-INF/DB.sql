af0e54fc0ccf995bd6173c83e8a29631c6552f82
HongSoonho <0113784358@naver.com>
2015-04-30 12:15:31 +0900

    aasd

--- PROJECT/WebContent/META-INF/DB.sql
+++ PROJECT/WebContent/META-INF/DB.sql
@@ -742,11 +742,11 @@ commit;
 
 --쪽지... 결제 확인... 어떻게 할것인가 현재 not null 이라 무조건 넣어 줘야함...
 insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150129,'홍순호','대표이사','true','true','true',100,1000,1);
-insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150130,'윤조영','개발부','true','true','true',200,2000,2);
-insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150131,'이상문','개발부','true','true','true',200,2001,3);
-insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150201,'추가영','개발부','true','true','true',200,2001,4);
-insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150214,'정자연','영업부','true','true','true',400,4001,4);
-insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150301,'이준하','영업부','true','true','true',400,4000,2);
+insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150130,'윤조영','개발부장','true','true','true',200,2000,2);
+insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150131,'이상문','솔루션팀','true','true','true',200,2001,3);
+insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150201,'추가영','솔루션팀','true','true','true',200,2001,4);
+insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150214,'정자연','기술경영팀','true','true','true',400,4001,4);
+insert into emp (empno,ename,teamname,signcheck,msgcheck,admin,deptcode,teamcode,grade)values(150301,'이준하','영업부장','true','true','true',400,4000,2);
 ---------------------------------------------------------------------------------------------(  사번 , 이름  , 팀이름 ,사인체크,메세지, 어드민,부서,팀코드,직급)   
 commit;
       
@@ -759,11 +759,6 @@ insert into domain_set (empno,emptel,hiredate,firedate,sex,birth,exist) values (
 -------------------------------------------------------------------------------( 사번  ,  사내전화번호 ,    입사일   ,퇴사일,성별 ,   생년월일  ,재직여부)
 commit;
 
-
---회원가입은 페이지에서 합시다.
---insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150129,1004,'0113784358@naver.com','서울시 성동구 행당동 317-130','010-5378-4358','사진경호','프로필내용'); 
---commit;
-
 insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150129,1004,'0asdfasd8@naver.com','서울시 성동구 행당동 317-130','010-5378-4358','사진경로','프로필내용');
 insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150130,1004,'01hthsh358@naver.com','동울시 동구 당동 317-130','010-5422-0028','사진경로','프로필내용'); 
 insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150131,1004,'01strh8@naver.com','남울시 구 당동 317-130','010-5958-1258','사진경로','프로필내용'); 
@@ -771,8 +766,17 @@ insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values 
 insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150214,1004,'01srth358@naver.com','굴울시 동 동 317-130','010-0743-4333','사진경로','프로필내용'); 
 insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150301,1004,'0123ya58@naver.com','박울시 성동 당동 317-130','010-5338-4348','사진경로','프로필내용'); 
 
+--회원가입은 페이지에서 합시다.
+--insert into accounts (empno,pwd,email,addr,celphone,p_picture,p_content) values (150129,1004,'0113784358@naver.com','서울시 성동구 행당동 317-130','010-5378-4358','사진경호','프로필내용'); 
+--commit;
+
+
 select * from emp;
 select * from domain_set;
 select * from accounts;
 select * from memberinfo;
+commit;
 
+desc accounts;
+
+select *  from c_brd;
