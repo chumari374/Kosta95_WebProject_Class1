ce29fe3bf7277d69d7025318ca1329e03c84580b
HongSoonho <0113784358@naver.com>
2015-05-06 09:54:07 +0900

    작업중

--- PROJECT/src/Action/Notice/CompNoticeAddAction.java
+++ PROJECT/src/Action/Notice/CompNoticeAddAction.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpSession;
 import Action.Action;
 import Action.ActionForward;
 import DAO.C_BrdDAO;
+import DAO.C_N_BrdDAO;
 import DTO.C_BrdDTO;
+import DTO.C_N_BrdDTO;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
@@ -21,29 +23,29 @@ public class CompNoticeAddAction implements Action {
 			throws Exception{
 
 		HttpSession session = request.getSession();
-		C_BrdDAO boarddao=new C_BrdDAO();
-		C_BrdDTO boarddto=new C_BrdDTO();
+		C_N_BrdDAO boarddao=new C_N_BrdDAO();
+		C_N_BrdDTO boarddto=new C_N_BrdDTO();
 		ActionForward forward=new ActionForward();
 
 		request.setCharacterEncoding("UTF-8");		   	
 		response.setCharacterEncoding("UTF-8");
 		boolean result=false;
-		String notice = "";
+//		String notice = "";
 		PrintWriter out = response.getWriter();
 
 		try{
 
-			if(request.getParameter("BOARD_NOTICE")!=null){
-				notice = "true";
-			}else{
-				notice = "false";
-			}
+//			if(request.getParameter("BOARD_NOTICE")!=null){
+//				notice = "true";
+//			}else{
+//				notice = "false";
+//			}
 
 			boarddto.setEmpno((int)session.getAttribute("empno"));
 			boarddto.setEname((String)session.getAttribute("ename"));
 			boarddto.setTitle(request.getParameter("BOARD_TITLE"));
 			boarddto.setContent(request.getParameter("BOARD_CONTENT"));
-			boarddto.setNotice(notice);
+		//	boarddto.setNotice(notice);
 
 			result=boarddao.Insert(boarddto);
 
@@ -55,7 +57,7 @@ public class CompNoticeAddAction implements Action {
 
 				System.out.println("게시판 등록 실패");
 				forward.setRedirect(true);
-				forward.setPath("./CompBoardList.cp");
+				forward.setPath("./CompNoticeList.cp");
 				return forward;
 			}
 
@@ -66,7 +68,7 @@ public class CompNoticeAddAction implements Action {
 			System.out.println("게시판 등록 완료");
 //			response.setCharacterEncoding("UTF-8");
 			forward.setRedirect(true);
-			forward.setPath("./CompBoardList.cp");
+			forward.setPath("./CompNoticeList.cp");
 			return forward;
 
 		}catch(Exception ex){
--- PROJECT/src/Action/Notice/CompNoticeDetailAction.java
+++ PROJECT/src/Action/Notice/CompNoticeDetailAction.java
@@ -5,18 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import Action.Action;
 import Action.ActionForward;
-import DAO.C_BrdDAO;
-import DTO.C_BrdDTO;
+import DAO.C_N_BrdDAO;
+import DTO.C_N_BrdDTO;
 
-public class CompBoardDetailAction implements Action {
+public class CompNoticeDetailAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
    		
-		C_BrdDAO boarddao=new C_BrdDAO();
-	   	C_BrdDTO boarddata=new C_BrdDTO();
+		C_N_BrdDAO boarddao=new C_N_BrdDAO();
+	   	C_N_BrdDTO boarddata=new C_N_BrdDTO();
 	   	
 		int num=Integer.parseInt(request.getParameter("num"));
 		boarddao.setReadCountUpdate(num);
@@ -31,7 +31,7 @@ public class CompBoardDetailAction implements Action {
 	   	request.setAttribute("boarddata", boarddata);
 	   	ActionForward forward = new ActionForward();
 	   	forward.setRedirect(false);
-	   	forward.setPath("./Comp/Comp_Board/Comp_Board_View.jsp");
+	   	forward.setPath("./Comp/Comp_Notice/Comp_Notice_View.jsp");
    		return forward;
 	}
 
