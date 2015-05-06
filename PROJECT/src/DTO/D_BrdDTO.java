59995c973317f8bddc0881aee1f1fb454f3fe3c8
HongSoonho <0113784358@naver.com>
2015-05-06 14:13:11 +0900

    부서게시판

--- PROJECT/src/DTO/D_BrdDTO.java
+++ PROJECT/src/DTO/D_BrdDTO.java
@@ -25,5 +25,129 @@ public class D_BrdDTO {
 	String notice;
 	int deptcode;
 	Date write_date;
+	int ref;
+	int dpth;
+	int step;
+	int count;
+	String ename;
+	
+	public D_BrdDTO() {
+	}
+	
+	public D_BrdDTO(int empno, int num, String title, String content,
+			String notice, int deptcode, Date write_date, int ref, int dpth,
+			int step, int count, String ename) {
+		super();
+		this.empno = empno;
+		this.num = num;
+		this.title = title;
+		this.content = content;
+		this.notice = notice;
+		this.deptcode = deptcode;
+		this.write_date = write_date;
+		this.ref = ref;
+		this.dpth = dpth;
+		this.step = step;
+		this.count = count;
+		this.ename = ename;
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getNotice() {
+		return notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+
+	public int getDeptcode() {
+		return deptcode;
+	}
+
+	public void setDeptcode(int deptcode) {
+		this.deptcode = deptcode;
+	}
+
+	public Date getWrite_date() {
+		return write_date;
+	}
+
+	public void setWrite_date(Date write_date) {
+		this.write_date = write_date;
+	}
+
+	public int getRef() {
+		return ref;
+	}
+
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+
+	public int getDpth() {
+		return dpth;
+	}
+
+	public void setDpth(int dpth) {
+		this.dpth = dpth;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
 	
 }
