f0a6532c4cacd6ac878f2b40b051793ab76032cc
Choo <Kayung>
2015-05-07 09:40:59 +0900

    MessageDTO

--- PROJECT/src/DTO/MessageDTO.java
+++ PROJECT/src/DTO/MessageDTO.java
@@ -1,5 +1,7 @@
 package DTO;
 
+import java.sql.Date;
+
 // =================쪽지==============
 public class MessageDTO {
 	private int msgnum;	//쪽지번호
@@ -8,18 +10,26 @@ public class MessageDTO {
 	private String content;	//내용
 	private String filepath;	//첨부파일
 	private int sendempno;	//받는사람
+	private Date write_date;
+	private int revdeletecheck;
+	private int sentdeletecheck;
 	
 	public MessageDTO(){}
 
 	public MessageDTO(int msgnum, int empno, String title, String content,
-			String filepath, int sendempno) {
+			String filepath, int sendempno, Date write_date, int revdeletecheck, int sentdeletecheck) {
 		this.msgnum = msgnum;
 		this.empno = empno;
 		this.title = title;
 		this.content = content;
 		this.filepath = filepath;
 		this.sendempno = sendempno;
+		this.write_date = write_date;
+		this.revdeletecheck = revdeletecheck;
+		this.sentdeletecheck = sentdeletecheck;
 	}
+	
+
 
 	public int getMsgnum() {
 		return msgnum;
@@ -68,5 +78,29 @@ public class MessageDTO {
 	public void setSendempno(int sendempno) {
 		this.sendempno = sendempno;
 	}
+	
+	public Date getWrite_date() {
+		return write_date;
+	}
+
+	public void setWrite_date(Date write_date) {
+		this.write_date = write_date;
+	}
+	public int getRevdeletecheck() {
+		return revdeletecheck;
+	}
+
+	public void setRevdeletecheck(int revdeletecheck) {
+		this.revdeletecheck = revdeletecheck;
+	}
+
+	public int getSentdeletecheck() {
+		return sentdeletecheck;
+	}
+
+	public void setSentdeletecheck(int sentdeletecheck) {
+		this.sentdeletecheck = sentdeletecheck;
+	}
+
 	
 }
